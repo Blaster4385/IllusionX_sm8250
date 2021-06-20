d30cd7494629fd6912f699fdc5c4f5d407ec0413
Pranav Vashi <neobuddy89@gmail.com>
2021-06-21 01:57:45 +0530

    techpack: oneplus: Add more gestures
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- drivers/oneplus/input/touchscreen/touchpanel_common_driver.c
+++ drivers/oneplus/input/touchscreen/touchpanel_common_driver.c
@@ -88,17 +88,21 @@ int pointx[2] = {0, 0};
 int pointy[2] = {0, 0};
 #define ABS(a,b) ((a - b > 0) ? a - b : b - a)
 
-uint8_t DouTap_enable = 0;				 // double tap
-uint8_t UpVee_enable  = 0;				 // V
-uint8_t LeftVee_enable = 0; 			 // >
-uint8_t RightVee_enable = 0;			 // <
-uint8_t Circle_enable = 0;				 // O
-uint8_t DouSwip_enable = 0; 			 // ||
-uint8_t Mgestrue_enable = 0;			 // M
-uint8_t Wgestrue_enable = 0;			 // W
-uint8_t Sgestrue_enable = 0;			 // S
-uint8_t SingleTap_enable = 0;			 // single tap
-uint8_t Enable_gesture = 0;
+static uint8_t DouTap_enable = 0;	        // double tap
+static uint8_t UpVee_enable  = 0;	        // V
+static uint8_t DownVee_enable = 0;		    // ^
+static uint8_t LeftVee_enable = 0; 			// >
+static uint8_t RightVee_enable = 0;			// <
+static uint8_t Circle_enable = 0;		    // O
+static uint8_t DouSwip_enable = 0; 			// ||
+static uint8_t Left2RightSwip_enable = 0;	// -->
+static uint8_t Right2LeftSwip_enable = 0;	// <--
+static uint8_t Up2DownSwip_enable = 0;	    // |v
+static uint8_t Down2UpSwip_enable = 0;	    // |^
+static uint8_t Mgestrue_enable = 0;			// M
+static uint8_t Wgestrue_enable = 0;			// W
+static uint8_t Sgestrue_enable = 0;			// S
+static uint8_t SingleTap_enable = 0;	    // single tap
 
 /*******Part2:declear Area********************************/
 static void speedup_resume(struct work_struct *work);
@@ -452,6 +456,10 @@ static void tp_gesture_handle(struct touchpanel_data *ts)
 			enabled = UpVee_enable;
 			key = KEY_GESTURE_DOWN_ARROW;
 			break;
+    	        case DownVee:
+			enabled = DownVee_enable;
+			key = KEY_GESTURE_UP_ARROW;
+			break;
 		case LeftVee:
 			enabled = LeftVee_enable;
 			key = KEY_GESTURE_RIGHT_ARROW;
@@ -467,6 +475,22 @@ static void tp_gesture_handle(struct touchpanel_data *ts)
 		case DouSwip:
 			enabled = DouSwip_enable;
 			key = KEY_GESTURE_TWO_SWIPE;
+			break;
+		case Left2RightSwip:
+			enabled = Left2RightSwip_enable;
+			key = KEY_GESTURE_SWIPE_RIGHT;
+			break;
+		case Right2LeftSwip:
+			enabled = Right2LeftSwip_enable;
+			key = KEY_GESTURE_SWIPE_LEFT;
+			break;
+		case Up2DownSwip:
+			enabled = Up2DownSwip_enable;
+			key = KEY_GESTURE_SWIPE_DOWN;
+			break;
+		case Down2UpSwip:
+			enabled = Down2UpSwip_enable;
+			key = KEY_GESTURE_SWIPE_UP;
 			break;
 		case Mgestrue:
 			enabled = Mgestrue_enable;
@@ -2572,10 +2596,11 @@ static DEVICE_ATTR(tp_fw_update, 0644, sec_update_fw_show, sec_update_fw_store);
 	{ \
 		int enabled = 0; \
 		char page[PAGESIZE] = {0}; \
-		copy_from_user(page, user_buf, count); \
-		sscanf(page, "%d", &enabled); \
-		out = enabled > 0 ? 1 : 0; \
-		return count; \
+		if (!copy_from_user(page, user_buf, count)) { \
+			sscanf(page, "%d", &enabled); \
+			out = enabled > 0 ? 1 : 0; \
+		} \
+                return count; \
 	} \
 	static const struct file_operations name##_enable_proc_fops = { \
 	    .write = name##_enable_write_func, \
@@ -2587,9 +2612,14 @@ static DEVICE_ATTR(tp_fw_update, 0644, sec_update_fw_show, sec_update_fw_store);
 GESTURE_ATTR(single_tap, SingleTap_enable);
 GESTURE_ATTR(double_tap, DouTap_enable);
 GESTURE_ATTR(down_arrow, UpVee_enable);
-GESTURE_ATTR(left_arrow, RightVee_enable);
+GESTURE_ATTR(up_arrow, DownVee_enable);
 GESTURE_ATTR(right_arrow, LeftVee_enable);
+GESTURE_ATTR(left_arrow, RightVee_enable);
 GESTURE_ATTR(double_swipe, DouSwip_enable);
+GESTURE_ATTR(down_swipe, Up2DownSwip_enable);
+GESTURE_ATTR(up_swipe, Down2UpSwip_enable);
+GESTURE_ATTR(right_swipe, Left2RightSwip_enable);
+GESTURE_ATTR(left_swipe, Right2LeftSwip_enable);
 GESTURE_ATTR(letter_o, Circle_enable);
 GESTURE_ATTR(letter_w, Wgestrue_enable);
 GESTURE_ATTR(letter_m, Mgestrue_enable);
@@ -2661,10 +2691,15 @@ static int init_touchpanel_proc(struct touchpanel_data *ts)
 	if (ts->black_gesture_support) {
 		CREATE_GESTURE_NODE(single_tap);
 		CREATE_GESTURE_NODE(double_tap);
+		CREATE_GESTURE_NODE(up_arrow);
 		CREATE_GESTURE_NODE(down_arrow);
 		CREATE_GESTURE_NODE(left_arrow);
 		CREATE_GESTURE_NODE(right_arrow);
 		CREATE_GESTURE_NODE(double_swipe);
+		CREATE_GESTURE_NODE(up_swipe);
+		CREATE_GESTURE_NODE(down_swipe);
+		CREATE_GESTURE_NODE(left_swipe);
+		CREATE_GESTURE_NODE(right_swipe);
 		CREATE_GESTURE_NODE(letter_o);
 		CREATE_GESTURE_NODE(letter_w);
 		CREATE_GESTURE_NODE(letter_m);
@@ -4237,9 +4272,14 @@ static int init_input_device(struct touchpanel_data *ts)
 		set_bit(KEY_DOUBLE_TAP, ts->input_dev->keybit);
 		set_bit(KEY_GESTURE_CIRCLE, ts->input_dev->keybit);
 		set_bit(KEY_GESTURE_TWO_SWIPE, ts->input_dev->keybit);
-		set_bit(KEY_GESTURE_DOWN_ARROW, ts->input_dev->keybit);
+		set_bit(KEY_GESTURE_UP_ARROW, ts->input_dev->keybit);
 		set_bit(KEY_GESTURE_LEFT_ARROW, ts->input_dev->keybit);
 		set_bit(KEY_GESTURE_RIGHT_ARROW, ts->input_dev->keybit);
+		set_bit(KEY_GESTURE_DOWN_ARROW, ts->input_dev->keybit);
+		set_bit(KEY_GESTURE_SWIPE_LEFT, ts->input_dev->keybit);
+		set_bit(KEY_GESTURE_SWIPE_DOWN, ts->input_dev->keybit);
+		set_bit(KEY_GESTURE_SWIPE_RIGHT, ts->input_dev->keybit);
+		set_bit(KEY_GESTURE_SWIPE_UP, ts->input_dev->keybit);
 		set_bit(KEY_GESTURE_SINGLE_TAP, ts->input_dev->keybit);
 	}
 

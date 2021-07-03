11d65b30c3dae70beab8cfeffca02fbda3feaaa1
YumeMichi <do4suki@gmail.com>
2021-07-04 00:54:03 +0800

    drm: msm: Reset to CAF LA.UM.9.12.r1-10800-SMxx50.0
    Signed-off-by: YumeMichi <do4suki@gmail.com>
    Change-Id: I5a1fe4b7461d33ccab367795efee6a3c3cc53a29

--- techpack/display/msm/msm_drv.h
+++ techpack/display/msm/msm_drv.h
@@ -107,8 +107,6 @@ enum msm_mdp_plane_property {
 
 	/* range properties */
 	PLANE_PROP_ZPOS = PLANE_PROP_BLOBCOUNT,
-	//xiaoxiaohuan@OnePlus.MultiMediaService,2018/08/04, add for fingerprint
-    PLANE_PROP_CUSTOM,
 	PLANE_PROP_ALPHA,
 	PLANE_PROP_COLOR_FILL,
 	PLANE_PROP_H_DECIMATE,
@@ -168,7 +166,6 @@ enum msm_mdp_crtc_property {
 	CRTC_PROP_IDLE_PC_STATE,
 
 	/* total # of properties */
-	CRTC_PROP_CUSTOM,
 	CRTC_PROP_COUNT
 };
 
@@ -204,7 +201,6 @@ enum msm_mdp_conn_property {
 	CONNECTOR_PROP_FB_TRANSLATION_MODE,
 	CONNECTOR_PROP_QSYNC_MODE,
 	CONNECTOR_PROP_CMD_FRAME_TRIGGER_MODE,
-	CONNECTOR_PROP_CUSTOM,
 
 	/* total # of properties */
 	CONNECTOR_PROP_COUNT
@@ -705,7 +701,6 @@ struct msm_drm_private {
 
 	/* update the flag when msm driver receives shutdown notification */
 	bool shutdown_in_progress;
-	ktime_t  commit_end_time;
 };
 
 /* get struct msm_kms * from drm_device * */

fe5b95b16e3f546ee8ec7bcac96a0753fbce30d5
TheMalachite <eliasgheeraert@gmail.com>
2022-06-18 12:39:18 +0200

    power: oplus: charger_ic: Fix headers path for SM8250 charger

--- drivers/power/oplus/charger_ic/oplus_battery_msm8250.c
+++ drivers/power/oplus/charger_ic/oplus_battery_msm8250.c
@@ -21,13 +21,13 @@
 #include <linux/pmic-voter.h>
 
 #ifdef OPLUS_FEATURE_CHG_BASIC
-#include "../../../../kernel/msm-4.19/drivers/power/supply/qcom/smb5-reg.h"
-#include "../../../../kernel/msm-4.19/drivers/power/supply/qcom/schgm-flash.h"
-#include "../../../../kernel/msm-4.19/drivers/power/supply/qcom/battery.h"
-#include "../../../../kernel/msm-4.19/drivers/power/supply/qcom/step-chg-jeita.h"
+#include "../../supply/qcom/smb5-reg.h"
+#include "../../supply/qcom/schgm-flash.h"
+#include "../../supply/qcom/battery.h"
+#include "../../supply/qcom/step-chg-jeita.h"
 #include "../voocphy/oplus_voocphy.h"
 #ifdef OPLUS_CUSTOM_OP_DEF
-#include "../../../../kernel/msm-4.19/drivers/power/supply/qcom/storm-watch.h"
+#include "../../supply/qcom/storm-watch.h"
 #endif
 #endif
 

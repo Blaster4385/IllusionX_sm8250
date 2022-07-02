237689830e3969b5609752a0dccbb1f2a8d7234c
Juhyung Park <qkrwngud825@gmail.com>
2022-07-03 03:09:18 +0900

    mm: compaction: allow using different proactiveness value depending on the screen state
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Change-Id: I94d15bd4da794d134a10095c4f432f6d77c8f32e

--- techpack/display/msm/dsi/dsi_display.c
+++ techpack/display/msm/dsi/dsi_display.c
@@ -7,6 +7,7 @@
 #include <linux/of.h>
 #include <linux/of_gpio.h>
 #include <linux/err.h>
+#include <linux/compaction.h>
 
 #include "msm_drv.h"
 #include "sde_connector.h"
@@ -1381,6 +1382,8 @@ static void _dsi_display_setup_misr(struct dsi_display *display)
 	}
 }
 
+bool dsi_screen_on __read_mostly = false;
+
 int dsi_display_set_power(struct drm_connector *connector,
 		int power_mode, void *disp)
 {
@@ -1398,6 +1401,8 @@ int dsi_display_set_power(struct drm_connector *connector,
 		break;
 	case SDE_MODE_DPMS_LP2:
 		rc = dsi_panel_set_lp2(display->panel);
+		WRITE_ONCE(dsi_screen_on, true);
+		wmb();
 		break;
 	case SDE_MODE_DPMS_ON:
 		if ((display->panel->power_mode == SDE_MODE_DPMS_LP1) ||
@@ -1405,6 +1410,10 @@ int dsi_display_set_power(struct drm_connector *connector,
 			rc = dsi_panel_set_nolp(display->panel);
 		break;
 	case SDE_MODE_DPMS_OFF:
+		WRITE_ONCE(dsi_screen_on, false);
+		wmb();
+		trigger_proactive_compaction(false);
+		break;
 	default:
 		return rc;
 	}
